23f37d592adf8e23c194f7775069c2963c8004b9
rlatjddms <145992500+rlatjddms@users.noreply.github.com>
2025-06-10 23:02:26 +0900

    Friend 기능 구현
    FriendRepository, FriendService

--- backend/src/main/java/project/planit/domain/Friend.java
+++ backend/src/main/java/project/planit/domain/Friend.java
@@ -1,8 +1,11 @@
 package project.planit.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter @Setter
 public class Friend {
 
     @Id
---
+++ backend/src/test/java/project/planit/service/FriendServiceTest.java
@@ -0,0 +1,40 @@
+package project.planit.service;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
+public class FriendServiceTest {
+
+    @Autowired FriendService friendService;
+
+    @Test
+    public void 친구_추가() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void 친구_초대() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    public void 친구_삭제() throws Exception {
+        //given
+
+        //when
+
+        //then
+    }
+}
